ca88a1ddedadcca28a15ff2becfb9a224f1b0ecb
Shyngys <56097088+Shyngysb@users.noreply.github.com>
2026-06-30 16:55:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ function updateTimer() {
 
 updateTimer();
 setInterval(updateTimer, 1000);
+
+// ===== SCROLL ANIMATION =====
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+sections.forEach(section => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
